54efbe824e365c396fe4a5a157dd797bc727b823
RajaRaviVarma <raja@thisisalice.com>
2020-09-09 21:03:42 +0530

    Modify timed_print.py to be more real

--- timed_print.py
+++ timed_print.py
@@ -1,5 +1,9 @@
+# This script is used to simulate some process which will print the same output
+# to the stdout at regular time intervals. An example of such a process is
+# vagrant rsync-auto.
+from random import randint
 from time import sleep
 
 for i in range(4):
-    print(f"Rsyncing folder {i}", flush=True)
-    sleep(5)
+    print(f"Rsyncing folders /app /tmp /config", flush=True)
+    sleep(randint(1, 6))
